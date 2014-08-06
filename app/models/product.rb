98ab558541712c508755914b5561389e61016300
Jonny <jonny@campbellhay.com>
2014-08-07 00:16:47 +0100

    update form

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,22 @@
   belongs_to :location
 
 
-  def self.get_kit(trip)
-    @products = Product.all
+  def self.get_kit(trip, categories)
+    destination = trip["destination"]
+    @category_list = {}
+    categories.each do |c|
+      product_list = []
+      c.products.includes(:location).where('locations.name LIKE ?', "#{destination}").each do |product|
+        if product.gender == trip.gender
+          product_list << product 
+        end
+      end
+
+      @category_list[c.name] = product_list
+      binding.pry
+    end
+
+    @category_list
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,52 +13,52 @@ france = Location.create(name: "France")
 england = Location.create(name: "England")
 
 c1 = Category.create(name: "Shampoo")
-p = Product.create(name: "AXE Shampoo Apollo", gender: "Male")
+p = Product.create(name: "AXE Shampoo Apollo", gender: "male")
 p.location = france
 p.category = c1
 p.save
-p2 = Product.create(name: "TIMOTEI Shampoo Pure", gender: "Male")
+p2 = Product.create(name: "TIMOTEI Shampoo Pure", gender: "male")
 p2.location = france
 p2.category = c1
 p2.save
-p3 = Product.create(name: "AXE Shampoo Just Clean", gender: "Male")
+p3 = Product.create(name: "AXE Shampoo Just Clean", gender: "male")
 p3.location = france
 p3.category = c1
 p3.save
-p4 = Product.create(name: "DOVE Shampoo Hair Therapy Oil Nutrition", gender: "Female")
+p4 = Product.create(name: "DOVE Shampoo Hair Therapy Oil Nutrition", gender: "female")
 p4.location = france
 p4.category = c1
 p4.save
-p5 = Product.create(name: "SUNSILK Shampooing Perfect Smoothing", gender: "Female")
+p5 = Product.create(name: "SUNSILK Shampooing Perfect Smoothing", gender: "female")
 p5.location = france
 p5.category = c1
 p5.save
-p6 = Product.create(name: "TONI & GUY Dry Shampoo", gender: "Female")
+p6 = Product.create(name: "TONI & GUY Dry Shampoo", gender: "female")
 p6.location = france
 p6.category = c1
 p6.save
 
-p2 = Product.create(name: "AXE Shampoo Apollo", gender: "Male")
+p2 = Product.create(name: "AXE Shampoo Apollo", gender: "male")
 p2.location = england
 p2.category = c1
 p2.save
-p22 = Product.create(name: "TIMOTEI Shampoo Pure", gender: "Male")
+p22 = Product.create(name: "TIMOTEI Shampoo Pure", gender: "male")
 p22.location = england
 p22.category = c1
 p22.save
-p23 = Product.create(name: "AXE Shampoo Just Clean", gender: "Male")
+p23 = Product.create(name: "AXE Shampoo Just Clean", gender: "male")
 p23.location = england
 p23.category = c1
 p23.save
-p24 = Product.create(name: "DOVE Shampoo Hair Therapy Oil Nutrition", gender: "Female")
+p24 = Product.create(name: "DOVE Shampoo Hair Therapy Oil Nutrition", gender: "female")
 p24.location = england
 p24.category = c1
 p24.save
-p25 = Product.create(name: "SUNSILK Shampooing Perfect Smoothing", gender: "Female")
+p25 = Product.create(name: "SUNSILK Shampooing Perfect Smoothing", gender: "female")
 p25.location = england
 p25.category = c1
 p25.save
-p26 = Product.create(name: "TONI & GUY Dry Shampoo", gender: "Female")
+p26 = Product.create(name: "TONI & GUY Dry Shampoo", gender: "female")
 p26.location = england
 p26.category = c1
 p26.save
@@ -66,37 +66,37 @@ p26.save
 
 
 c2 = Category.create(name: "Hair Styling")
-p7 = Product.create(name: "AXE Apollo Gel Axis", gender: "Male")
+p7 = Product.create(name: "AXE Apollo Gel Axis", gender: "male")
 p7.location = france
 p7.category = c2
 p7.save
-p8 = Product.create(name: "AXE Styling Gel Hot Night Long", gender: "Male")
+p8 = Product.create(name: "AXE Styling Gel Hot Night Long", gender: "male")
 p8.location = france
 p8.category = c2
 p8.save
-p9 = Product.create(name: "TONI & GUY Salt Spray Beach Effect", gender: "Female")
+p9 = Product.create(name: "TONI & GUY Salt Spray Beach Effect", gender: "female")
 p9.location = france
 p9.category = c2
 p9.save
-p10 = Product.create(name: "TONI & GUY Foam Air", gender: "Female")
+p10 = Product.create(name: "TONI & GUY Foam Air", gender: "female")
 p10.location = france
 p10.category = c2
 p10.save
 
 
-p17 = Product.create(name: "AXE Apollo Gel Axis", gender: "Male")
+p17 = Product.create(name: "AXE Apollo Gel Axis", gender: "male")
 p17.location = england
 p17.category = c2
 p17.save
-p18 = Product.create(name: "AXE Styling Gel Hot Night Long", gender: "Male")
+p18 = Product.create(name: "AXE Styling Gel Hot Night Long", gender: "male")
 p18.location = england
 p18.category = c2
 p18.save
-p19 = Product.create(name: "TONI & GUY Salt Spray Beach Effect", gender: "Female")
+p19 = Product.create(name: "TONI & GUY Salt Spray Beach Effect", gender: "female")
 p19.location = england
 p19.category = c2
 p19.save
-p110 = Product.create(name: "TONI & GUY Foam Air", gender: "Female")
+p110 = Product.create(name: "TONI & GUY Foam Air", gender: "female")
 p110.location = england
 p110.category = c2
 p110.save

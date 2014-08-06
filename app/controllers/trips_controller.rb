1b80b3826bd701f518225ed258efddf3a690a4d1
Jonny Adshead <jonny.adshead@gmail.com>
2014-08-06 18:59:07 +0100

    starting to present data

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,8 +1,14 @@
 class TripsController < ApplicationController
 
   def index
+    @category_list = []
+    @categories = Category.all
+    @categories.each do |c|
+      @category_list << c.name
+    end
     @products = Product.get_kit(@trip)
   end
+
   def create
     @trip = Trip.create(params[:trip])
     redirect_to trip: :index
